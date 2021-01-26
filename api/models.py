586caac5f4c387218f71941852ab64337ce39372
Batool Malkawi <batoolmalkawii@gmail.com>
2021-01-26 17:30:24 +0200

    Done

---
+++ api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.TextField(max_length = 32 , blank =False , null = False)
+
+    def __str__(self):
+        return self.title
